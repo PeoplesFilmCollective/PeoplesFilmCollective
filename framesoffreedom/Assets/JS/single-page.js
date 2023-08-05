38f6d3bee119f463bae99fad4aad221061bd3c23
miraz-mondal <mirazmondal.official@gmail.com>
2023-08-05 17:45:37 +0530

    route and films page infinity scrolling fixed

--- framesoffreedom/Assets/JS/single-page.js
+++ framesoffreedom/Assets/JS/single-page.js
@@ -1,8 +1,8 @@
 const slider = document.querySelector('.slider');
 const slidesContainer = document.querySelector('.slides-container');
-
-// Duplicate images to create the infinite loop
-slidesContainer.innerHTML += slidesContainer.innerHTML;
+const images = slidesContainer.querySelectorAll('img');
+const slideWidth = images[0].clientWidth;
+const totalSlides = images.length;
 
 // Optional: If you want to pause the slider when the user hovers over it.
 slider.addEventListener('mouseenter', () => {
@@ -11,4 +11,17 @@ slider.addEventListener('mouseenter', () => {
 
 slider.addEventListener('mouseleave', () => {
   slider.style.animationPlayState = 'running';
+});
+
+// Stop the animation after the last image in the original set
+slider.addEventListener('animationiteration', () => {
+  const currentTranslateX = parseFloat(window.getComputedStyle(slidesContainer).getPropertyValue('transform').split(',')[4]);
+  const distanceToEnd = slideWidth * totalSlides - Math.abs(currentTranslateX);
+  
+  if (distanceToEnd < slideWidth) {
+    // Reset the slider to the beginning
+    slidesContainer.style.animation = 'none';
+    slidesContainer.offsetHeight; // Trigger reflow to reset the animation
+    slidesContainer.style.animation = 'scrollImages 20s linear infinite';
+  }
 });
