e7ec20d545033c31011a7e006656f0bd42732d1e
miraz-mondal <mirazmondal.official@gmail.com>
2023-08-07 18:51:06 +0530

    image carousel added in the single film page

--- framesoffreedom/Assets/JS/single-page.js
+++ framesoffreedom/Assets/JS/single-page.js
@@ -1,27 +1,73 @@
-const slider = document.querySelector('.slider');
-const slidesContainer = document.querySelector('.slides-container');
-const images = slidesContainer.querySelectorAll('img');
-const slideWidth = images[0].clientWidth;
-const totalSlides = images.length;
+const carousel = document.querySelector(".carousel"),
+firstImg = carousel.querySelectorAll("img")[0],
+arrowIcons = document.querySelectorAll(".wrapper i");
 
-// Optional: If you want to pause the slider when the user hovers over it.
-slider.addEventListener('mouseenter', () => {
-  slider.style.animationPlayState = 'paused';
+let isDragStart = false, isDragging = false, prevPageX, prevScrollLeft, positionDiff;
+
+const showHideIcons = () => {
+    // showing and hiding prev/next icon according to carousel scroll left value
+    let scrollWidth = carousel.scrollWidth - carousel.clientWidth; // getting max scrollable width
+    arrowIcons[0].style.display = carousel.scrollLeft == 0 ? "none" : "block";
+    arrowIcons[1].style.display = carousel.scrollLeft == scrollWidth ? "none" : "block";
+}
+
+arrowIcons.forEach(icon => {
+    icon.addEventListener("click", () => {
+        let firstImgWidth = firstImg.clientWidth + 14; // getting first img width & adding 14 margin value
+        // if clicked icon is left, reduce width value from the carousel scroll left else add to it
+        carousel.scrollLeft += icon.id == "left" ? -firstImgWidth : firstImgWidth;
+        setTimeout(() => showHideIcons(), 60); // calling showHideIcons after 60ms
+    });
 });
 
-slider.addEventListener('mouseleave', () => {
-  slider.style.animationPlayState = 'running';
-});
+const autoSlide = () => {
+    // if there is no image left to scroll then return from here
+    if(carousel.scrollLeft - (carousel.scrollWidth - carousel.clientWidth) > -1 || carousel.scrollLeft <= 0) return;
 
-// Stop the animation after the last image in the original set
-slider.addEventListener('animationiteration', () => {
-  const currentTranslateX = parseFloat(window.getComputedStyle(slidesContainer).getPropertyValue('transform').split(',')[4]);
-  const distanceToEnd = slideWidth * totalSlides - Math.abs(currentTranslateX);
-  
-  if (distanceToEnd < slideWidth) {
-    // Reset the slider to the beginning
-    slidesContainer.style.animation = 'none';
-    slidesContainer.offsetHeight; // Trigger reflow to reset the animation
-    slidesContainer.style.animation = 'scrollImages 20s linear infinite';
-  }
-});
+    positionDiff = Math.abs(positionDiff); // making positionDiff value to positive
+    let firstImgWidth = firstImg.clientWidth + 14;
+    // getting difference value that needs to add or reduce from carousel left to take middle img center
+    let valDifference = firstImgWidth - positionDiff;
+
+    if(carousel.scrollLeft > prevScrollLeft) { // if user is scrolling to the right
+        return carousel.scrollLeft += positionDiff > firstImgWidth / 3 ? valDifference : -positionDiff;
+    }
+    // if user is scrolling to the left
+    carousel.scrollLeft -= positionDiff > firstImgWidth / 3 ? valDifference : -positionDiff;
+}
+
+const dragStart = (e) => {
+    // updatating global variables value on mouse down event
+    isDragStart = true;
+    prevPageX = e.pageX || e.touches[0].pageX;
+    prevScrollLeft = carousel.scrollLeft;
+}
+
+const dragging = (e) => {
+    // scrolling images/carousel to left according to mouse pointer
+    if(!isDragStart) return;
+    e.preventDefault();
+    isDragging = true;
+    carousel.classList.add("dragging");
+    positionDiff = (e.pageX || e.touches[0].pageX) - prevPageX;
+    carousel.scrollLeft = prevScrollLeft - positionDiff;
+    showHideIcons();
+}
+
+const dragStop = () => {
+    isDragStart = false;
+    carousel.classList.remove("dragging");
+
+    if(!isDragging) return;
+    isDragging = false;
+    autoSlide();
+}
+
+carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("touchstart", dragStart);
+
+document.addEventListener("mousemove", dragging);
+carousel.addEventListener("touchmove", dragging);
+
+document.addEventListener("mouseup", dragStop);
+carousel.addEventListener("touchend", dragStop);
